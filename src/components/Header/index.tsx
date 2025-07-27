44f253fb634583382fa9f9151b0be1ad7d670242
ounjuu <koj1555@naver.com>
2025-07-27 15:25:18 +0900

    Feat: React.memo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 // Header/Header.jsx
+import React from "react";
 import { HeaderContainer } from "./styled";
 
-const Header = () => {
+const Header = React.memo(() => {
   return (
     <HeaderContainer>
       <h1>Drawing</h1>
     </HeaderContainer>
   );
-};
-
+});
 export default Header;
